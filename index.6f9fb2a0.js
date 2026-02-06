75cd007444108ec7f85895070b2852d125e100f5
Natalia Panchuk <Foreverdanda@ukr.net>
2026-02-06 18:27:03 +0200

    make build

---
+++ index.6f9fb2a0.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelectorAll("tr")).forEach(function(e){if(e.children.length>1){var r=e.children[e.childElementCount-1],n=e.children[1].cloneNode(!0);e.insertBefore(n,r)}});
+//# sourceMappingURL=index.6f9fb2a0.js.map
