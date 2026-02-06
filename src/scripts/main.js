3b8c01c4739238619c77da7617733e4aace678bd
Natalia Panchuk <Foreverdanda@ukr.net>
2026-02-06 18:26:10 +0200

    add task solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const tr = document.querySelectorAll('tr');
 
 Array.from(tr).forEach((row) => {
-  if (row.childElementCount >= 1) {
+  if (row.children.length > 1) {
     const pasteBeforeEl = row.children[row.childElementCount - 1];
     const sourceClone = row.children[1];
     const copyCell = sourceClone.cloneNode(true);
